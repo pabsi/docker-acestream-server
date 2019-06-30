ed10c13133facbd1363f9af405531e14c3555849
Pablo <pablo@moo.com>
2019-06-30 01:38:45 +0100

    Added CI/CD

--- Makefile
+++ Makefile
@@ -3,12 +3,18 @@ SHELL := /usr/bin/env bash
 MAKEFLAGS += s
 
 APP_NAME := acestream-server
+VERSION := 3.1.16
 
 build:
-	docker build --compress -t ${APP_NAME} .
+	docker build --compress -t ${APP_NAME} . && \
+	docker tag acestream-server pabsi/acestream-server:$VERSION
 
 test:
-	docker run --rm --name ${APP_NAME} -p 6878:6878 -it ${APP_NAME}
+	docker run --rm --name ${APP_NAME} -P -it ${APP_NAME}
 
 shell:
 	docker exec -it ${APP_NAME} bash
+
+push:
+	docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+	docker push pabsi/acestream-server:$VERSION
---
+++ play.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+echo -n "Starting ACE Stream Server... "
+docker run -d --rm --name acestream-server -p 6878:6878 -it acestream-server || exit 1
+echo "ACE Stream Server started. Waiting for it to be ready..."
+
+while true
+do
+	state="$(docker inspect --format='{{ json . }}' acestream-server | jq -r .State.Health.Status)"
+	if [ "$state" = "healthy" ]
+	then
+		break
+	fi
+	sleep 0.5
+done
+
+echo "ACE Stream Server is now ready and listening for connections. Launching ACE Stream Player..."
+acestream-launcher -p vlc "$1"
+
+echo -n "Stopping ACE Stream Server... "
+docker rm -f acestream-server
+
+exit $?
